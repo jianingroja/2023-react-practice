58cf3a657307d8d43f8daf7d43abc1749cd609dc
wangjianing <jianingw321@outlook.com>
2023-05-05 12:18:34 +0200

    display date

--- src/DateNumber.js
+++ src/DateNumber.js
@@ -5,9 +5,31 @@ const DateNumber = () => {
   const dateConstructor = new Date();
   const date = dateConstructor.getDate();
 
+  //todo separate component
+  // array for passed date
+  let passedDateArray = [];
+  for (let i = 1; i < date; i++) {
+    passedDateArray.push(i);
+  }
+
   return (
     <div>
-      <h2 className="date-number"> {date}</h2>
+      <div className="passed-date">
+        {/* passed date */}
+        {passedDateArray.map((date, index) => (
+          <h2 className="passed-date-number" key={index}>
+            {date}
+          </h2>
+        ))}
+        {/* <h2 className="passed-date-number">1</h2>
+        <h2 className="passed-date-number">2</h2>
+        <h2 className="passed-date-number">3</h2> */}
+      </div>
+      {/* margin from h2 doesn't collapse between these 2 div, needs to be fixed. */}
+      <div>
+        {/* present date */}
+        <h2 className="present-date-number">{date}</h2>
+      </div>
     </div>
   );
 };
